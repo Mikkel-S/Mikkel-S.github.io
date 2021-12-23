e93483721de59aff118c1723e3c2088a66703b96
Mikkel Stender <mikkel-s.mikkelstender@gmail.com>
2021-12-23 22:12:12 +0100

    new function

--- page_scripts/calculatorscript.js
+++ page_scripts/calculatorscript.js
@@ -1,4 +1,7 @@
-function putElement() {
+function putElement(elmt) {
+  var element = document.getElementById(elmt).innerHTML
   document.getElementById("output_paragraph").innerHTML =
     "Yes, you clicked the button!";
+  alert(element)
 }
+
