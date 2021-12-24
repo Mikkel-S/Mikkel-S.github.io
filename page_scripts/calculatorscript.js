a889f61f9b322112ef2981d181094c587ef54654
Mikkel Stender <mikkel-s.mikkelstender@gmail.com>
2021-12-24 13:07:38 +0100

    troubleshooting this

--- page_scripts/calculatorscript.js
+++ page_scripts/calculatorscript.js
@@ -1,7 +1,4 @@
 function putElement(elmt) {
-  var element = document.getElementById(elmt).innerHTML
-  document.getElementById("output_paragraph").innerHTML =
-    "Yes, you clicked the button!";
-  alert(element)
+  alert(elmt)
+  document.getElementById("output_paragraph").innerHTML = elmt
 }
-
